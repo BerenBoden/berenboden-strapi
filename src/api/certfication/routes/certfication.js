4ea327c23c45606b6836f5d7cfb9757b146db8b1
berenboden <beren.boden@gmail.com>
2023-06-22 09:06:23 +1200

    Initial commit with collection types

---
+++ src/api/certfication/routes/certfication.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * certfication router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::certfication.certfication');
---
+++ src/api/certfication/services/certfication.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * certfication service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::certfication.certfication');
