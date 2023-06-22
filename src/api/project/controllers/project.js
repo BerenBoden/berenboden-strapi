e48b2abdbcf81b6a9da58a7f3f44437dfccc8bfd
berenboden <beren.boden@gmail.com>
2023-06-23 11:10:58 +1200

    Deleted the createdBy and updatedBy values from the results of /project/slug, as they contain my hashed password and user email, meaning this is an information disclosure vulnerabillity, where an application unintentionally reveals sensitive information.

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -15,6 +15,11 @@ module.exports = createCoreController("api::project.project", ({ strapi }) => ({
     const entity = await strapi.service("api::project.project").find(query);
     const { results } = await this.sanitizeOutput(entity, ctx);
 
+    results.forEach((result) => {
+      delete result.updatedBy;
+      delete result.createdBy;
+    });
+
     return this.transformResponse(results[0]);
   },
 }));
