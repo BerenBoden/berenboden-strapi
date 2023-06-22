794bc810a66e445cdb272ba36946121bddea045a
berenboden <beren.boden@gmail.com>
2023-06-23 11:30:06 +1200

    Added findOne by slug function to all collection-types article, certification, projects

--- src/api/certification/controllers/certification.js
+++ src/api/certification/controllers/certification.js
@@ -1,9 +1,30 @@
-'use strict';
+"use strict";
 
 /**
  * certification controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::certification.certification');
+module.exports = createCoreController(
+  "api::certification.certification",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id: slug } = ctx.params;
+      const { query } = ctx;
+      if (!query.filters) query.filters = {};
+      query.filters.slug = { $eq: slug };
+      const entity = await strapi
+        .service("api::certification.certification")
+        .find(query);
+      const { results } = await this.sanitizeOutput(entity, ctx);
+
+      results.forEach((result) => {
+        delete result.updatedBy;
+        delete result.createdBy;
+      });
+
+      return this.transformResponse(results[0]);
+    },
+  })
+);
