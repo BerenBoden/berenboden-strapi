50bd5f83c4a87ffc40cbe9036c4a846df1845345
berenboden <beren.boden@gmail.com>
2023-10-14 11:28:28 +1300

    Audited packages and updated to fix vulnerabilities

---
+++ types/generated/components.d.ts
@@ -0,0 +1,24 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface ResourceResourceData extends Schema.Component {
+  collectionName: 'components_resource_resource_data';
+  info: {
+    displayName: 'ResourceData';
+    description: '';
+  };
+  attributes: {
+    content: Attribute.RichText & Attribute.Required;
+    cover: Attribute.Media & Attribute.Required;
+    description: Attribute.String & Attribute.Required;
+    external: Attribute.String;
+    featured: Attribute.Boolean;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'resource.resource-data': ResourceResourceData;
+    }
+  }
+}
